c43730e98582acb7824a51a5058f937fa9a7a6cb
Qui la Que di <qui.la.que.di@gmail.com>
2023-10-22 14:11:42 +0100

    Add delete-text command

---
+++ scripts/delete-text.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from models import db, Text
+from loguru import logger
+
+DB_PATH = Path(__file__).parent / "data" / "wordtrail.db"
+
+parser = ArgumentParser(description="Removes a text from the corpus.")
+parser.add_argument("--title", type=str, required=False)
+parser.add_argument("--database", type=Path, default=DB_PATH)
+args = parser.parse_args()
+
+# Connect to the database
+db.init(args.database)
+db.connect()
+
+# Delete text from the database
+text = Text.get(Text.title == args.title)
+text.delete_instance(recursive=True)
+logger.info(f"{args.title} and all words contained deleted from {args.database}.")
+
+# Close database connection
+db.close()
---
+++ scripts/tests/test_delete_text.py
@@ -0,0 +1,75 @@
+import pytest
+import subprocess
+
+from pathlib import Path
+from ..models import db, Text, Word, WordComparison
+
+TEST_DIR = Path(__file__).parent
+
+@pytest.fixture(scope="module")
+def setup_test_db():
+    # Create test sqlite db
+    db_path = TEST_DIR/"data"/"wordtrail.db"
+    args = [
+        "python",
+        TEST_DIR.parent / "init-stores.py",
+        "--db_path",
+        db_path
+    ]
+    subprocess.run(args, check=True)
+    # Initialize test db
+    db.init(db_path)
+
+    yield db_path
+
+    # Teardown test db
+    subprocess.run(["rm", db_path], check=True)
+
+
+def test_delete_text(setup_test_db):
+    test_db = setup_test_db
+    # Add texts to database
+    post_1_args = [
+        "python",
+        TEST_DIR.parent / "post-text.py",
+        "--filepath",
+        TEST_DIR/"data"/"source_texts"/"sample.txt",
+        "--language",
+        "EN",
+        "--database",
+        test_db
+    ]
+    subprocess.run(post_1_args, check=True)
+
+    post_2_args = [
+        "python",
+        TEST_DIR.parent / "post-text.py",
+        "--filepath",
+        TEST_DIR/"data"/"source_texts"/"Text 1.txt",
+        "--language",
+        "EN",
+        "--database",
+        test_db
+    ]
+    subprocess.run(post_2_args, check=True)
+
+    # Run command
+    DELETED_TITLE = "sample"
+    args = [
+        "python",
+        TEST_DIR.parent / "delete-text.py",
+        "--title",
+        DELETED_TITLE,
+        "--database",
+        test_db
+    ]
+    subprocess.run(args, check=True)
+
+    db.init(test_db)
+    db.connect()
+
+    # Check correct entries deleted
+    assert Text.select().where(Text.title == DELETED_TITLE).count() == 0
+    assert Text.select().count() == 1
+
+    db.close()
