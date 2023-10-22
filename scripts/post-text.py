3964655fce576d9022027089a7f081612526958a
Qui la Que di <qui.la.que.di@gmail.com>
2023-10-22 18:58:12 +0100

    🚀 Batch text data insertion

--- scripts/post-text.py
+++ scripts/post-text.py
@@ -5,9 +5,11 @@ import re
 
 from models import db, Text, Word, WordComparison
 from loguru import logger
+from tqdm import tqdm
 
 DB_PATH = Path(__file__).parent / "data" / "wordtrail.db"
 LANGUAGE_CODES_PATH = Path(__file__).parent / "data" / "languages.json"
+INSERT_BATCH_SIZE = 60_000
 
 
 def clean_word(word: str):
@@ -15,6 +17,16 @@ def clean_word(word: str):
         return word
     else:
         return re.sub(pattern=r"^\W*(.*\w)\W*$", repl=r"\1", string=word.lower())
+
+
+def compare_words(word_pair) -> WordComparison:
+    base_word, comp_word = word_pair
+    match_comparison = base_word["normal_form"] == comp_word["normal_form"]
+    return WordComparison(
+        base_id=base_word["id"],
+        comp_id=comp_word["id"],
+        is_match=match_comparison,
+    )
 
 
 parser = ArgumentParser(description="Adds a text to the corpus.")
@@ -61,43 +73,40 @@ text.save()
 logger.info(f"Text record for {text_title} created.")
 
 # Add words to the database
+word_count = 0
 for word in words:
     word.text_id = text.id
-    word.save()
-logger.info(f"{len(words)} word records created for {text_title}.")
+    word_count += 1
+with db.atomic():
+    Word.bulk_create(words, batch_size=INSERT_BATCH_SIZE)
+logger.info(f"{word_count} word records created for {text_title}.")
 
 # Make word comparisons
-all_words = Word.select()
-within_text_comparisons = [
-    {
-        "base_word": base_word,
-        "comp_word": comp_word,
-        "is_match": (base_word.normal_form == comp_word.normal_form),
-    }
-    for base_word in words
-    for comp_word in words
-    if base_word.text_pos < comp_word.text_pos
-]
-across_text_comparisons = [
-    {
-        "base_word": base_word,
-        "comp_word": comp_word,
-        "is_match": (base_word.normal_form == comp_word.normal_form),
-    }
+
+# SQLite objects can't be used in another thread, so load words as dicts
+all_words = [x for x in Word.select().dicts()]
+new_words = [x for x in text.words.dicts()]  # Load inserted words to get their ids.
+
+within_text_pairs = (
+    (base_word, comp_word)
+    for base_word in new_words
+    for comp_word in new_words
+    if base_word["text_pos"] < comp_word["text_pos"]
+)
+across_text_pairs = (
+    (base_word, comp_word)
     for base_word in all_words
-    for comp_word in words
-    if base_word.text_id != comp_word.text_id
-]
-comparisons = within_text_comparisons + across_text_comparisons
+    for comp_word in new_words
+    if base_word["text_id"] != comp_word["text_id"]
+)
 
 # Add word comparisons to the database
-for comparison in comparisons:
-    WordComparison(
-        base_id=comparison["base_word"].id,
-        comp_id=comparison["comp_word"].id,
-        is_match=comparison["is_match"],
-    ).save()
-logger.info(f"{len(comparisons)} word comparison records created for {text_title}.")
+within_text_comparisons = tqdm(map(compare_words, within_text_pairs))
+across_text_comparisons = tqdm(map(compare_words, across_text_pairs))
+with db.atomic():
+    WordComparison.bulk_create(within_text_comparisons, batch_size=INSERT_BATCH_SIZE)
+    WordComparison.bulk_create(across_text_comparisons, batch_size=INSERT_BATCH_SIZE)
+logger.info(f"Word comparison records created for {text_title}.")
 
 
 # Close database connection
