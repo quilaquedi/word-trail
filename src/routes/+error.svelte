d5b8e2fae82364b76be878c666e035724c4fc653
Qui la Que di <qui.la.que.di@gmail.com>
2023-10-25 22:30:59 +0100

    ✨ Nicer error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script type="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="hero min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<p class="text-accent text-7xl">{$page.error.message}</p>
+			<a class="my-5 btn btn-accent" href="/">Go home</a>
+		</div>
+	</div>
+</div>
